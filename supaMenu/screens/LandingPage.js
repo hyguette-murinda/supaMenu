a26a450fda5e8f243107fa1eb8efb11b06d2ecf6
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-05-12 11:50:22 +0200

    landing complete

--- supaMenu/screens/LandingPage.js
+++ supaMenu/screens/LandingPage.js
@@ -1,11 +1,13 @@
-import { StyleSheet } from "react-native";
 import tailwind from "twrnc";
-import { Text, View } from "../components/Themed";
+import { View, Text, Pressable } from "react-native";
 
 export default function LandingPage() {
   return (
     <View style={tailwind`flex-1 w-full items-center justify-center bg-amber-700`}>
-      <Text>SupaMenu</Text>
+      <Text>
+        <Text style={[tailwind`text-black bold text-xl`]}>Supa</Text>
+        <Text style={[tailwind`text-white bold text-xl`]}>Menu</Text>
+      </Text>
     </View>
   );
 }
