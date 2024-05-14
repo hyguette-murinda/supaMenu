61af0da38034ef46089e8c3ae666d286785ce12e
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-05-14 20:47:45 +0200

    resto card component

---
+++ supaMenu/components/NearRestoComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const NearbyRestoCard = ({ name, location, imageSource }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={imageSource} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.location}>{location}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '80%',
+    height: '30%',
+    flexDirection: 'row',
+    borderRadius: 10,
+    backgroundColor: 'white',
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  image: {
+    width: '40%',
+    height: '100%',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  detailsContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  location: {
+    fontSize: 14,
+  },
+});
+
+export default NearbyRestoCard;
